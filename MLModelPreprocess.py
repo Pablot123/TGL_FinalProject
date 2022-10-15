312f6f35f270b3f107862d4ed83c201b52615c70
Pablot123 <pablotamayo44@gmail.com>
2022-10-14 21:18:07 -0500

    Update MLModelPreprocess.py

--- MLModelPreprocess.py
+++ MLModelPreprocess.py
@@ -1,10 +1,7 @@
 from sklearn.feature_extraction.text import CountVectorizer
-
-
 import nltk 
 from nltk.corpus import stopwords 
 import re
-
 
 stemmer = nltk.stem.SnowballStemmer('english') # Vamos a utlizar el Snowball Stemmer para realizar stemming (nos permite llevar las palabras a una forma estandar). 
 nltk.download('stopwords') # Lista de palabras de parada en ingles.
@@ -16,26 +13,37 @@ class DataPreprocess:
 
 
     def processing_text(self, texto):
+        '''
+        Obtiene como entrada un texto y retorna el texto procesado
+        '''
         #Remover con un expresión regular carateres especiales (no palabras).
         processed_feature = re.sub(r'\W', ' ', str(texto))
+
         #Remover ocurrencias de caracteres individuales
         processed_feature= re.sub(r'\s+[a-zA-Z]\s+', ' ', processed_feature)
         processed_feature = re.sub(r'\^[a-zA-Z]\s+', ' ', processed_feature) 
-        #Remover números (Ocurrencias muy esporádicas en nuestro dataset)
+
+        #Remover números
         processed_feature = re.sub(r'[0-9]+', ' ', processed_feature)
+
         #Simplificar espacios concecutivos a un único espacio entre palabras
         processed_feature = re.sub(' +', ' ', processed_feature)
+
         #Pasar todo el texto a minúsculas    
         processed_feature = processed_feature.lower()
+
         #Aplicar stemming. Es una forma de enviar las palabras a una raiz común simplificando de esta manera el vocabulario. 
         #y de esta forma se evita tener dos palabras diferentes con el mismo significado en nuestro vocabulario.
-        #Ademas de eliminar las palabras que tengan menos de dos letras
+        #Ademas de eliminar las palabras que tengan menos de tres letras
         processed_feature = " ".join([stemmer.stem(i) for i in processed_feature.split() if len(i) >=3])
         
-
         return processed_feature
     
     def convert_data(self):
+        '''
+        Convierte todo el conjunto de dataset retornando 
+        el mismo conjunto con el texto procesado
+        '''
         raw_data = self.data['review'].values
         texto_procesado = []
         for sentence in raw_data:
@@ -44,6 +52,10 @@ class DataPreprocess:
         return texto_procesado
 
     def vectroize_data(self, texto_procesado):
+        '''
+        Recibe los datos procesados.
+        retorna el objeto vectorizer creado y la matriz que se genera al vectorizar los datos
+        '''
         vectorizer = CountVectorizer(max_features=2500, stop_words=stopwords.words('english'))
         # max_features representa el tamaño del vocabulario. Vamos a permitir 2500 palabras.
         # stop_words le indicamos las palabras de parada para que las ignore en el vocabulario. 
